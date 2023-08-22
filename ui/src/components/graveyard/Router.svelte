d05ea473bf8f546734b5db1133134e9d74770fb2
Mac Heller-Ogden <mac@machellerogden.com>
2023-08-22 16:50:08 -0500

    chore: cleanup

--- ui/src/components/graveyard/Router.svelte
+++ ui/src/components/graveyard/Router.svelte
@@ -1,48 +1,89 @@
-<script>
-    import { location } from './location.js';
-
-    let component;
-
-    // [ [ <routeExpression>, <Component> ] ]
-    export let routes = new Map();
-
-    function is(routeExpression, location) {
-        let pass = false;
-        if (routeExpression instanceof RegExp) {
-            pass = routeExpression.test(location.pathname);
-        } else {
-            pass = routeExpression == location.pathname;
-        }
-        return pass;
-    }
-
-    function match(location, routes) {
-        for (const [ routeExpression, Component ] of routes) {
-            if (is(routeExpression, location)) {
-                component = Component;
-                break;
-            }
-        }
-    }
-
-    $: pathname = $location?.pathname;
-
-    $: {
-        if (pathname) match($location, routes);
-    }
-
-</script>
-
-<svelte:component this={component} />
-
 <!--
-<script>
-    import Router from './routes/Router.svelte';
-    const routes = new Map([
-        [ '/', Conversation ],
-        [ '/home', Conversation ]
-    ]);
-</script>
-<Router {routes} />
+    Minimalist HashRouter - no history, etc.
+
+    Usage:
+
+    <script>
+        import Router from './routes/Router.svelte';
+        const routes = new Map([
+            [ '/', Conversation ],
+            [ '/home', Conversation ]
+        ]);
+    </script>
+
+    <Router {routes} />
+
 -->
 
+<script context="module">
+    import { writable } from 'svelte/store';
+
+    // just to make this work from inside the svelte.dev's iframe
+    const _window = window.parent;
+
+    export const createVirtualLocation = (location = _window.location) => {
+        const url = new URL((url => url.origin + url.hash.substring(1))(location));
+        console.log(`new virtual location: ${url}`);
+        return url;
+    }
+
+    const virtualLocation = writable(createVirtualLocation()); // use parent for demo
+
+    export const location = {
+        subscribe(cb) {
+            const hashChangeHandler = event => {
+                if (!event.newURL.includes('#/')) {
+                    cb(new URL(window.parent.location.origin + '#/'))
+                    return;
+                }
+                cb(createVirtualLocation(new URL(event.newURL)));
+            };
+            _window.addEventListener('hashchange', hashChangeHandler);
+            const _locationUnsubscribe = virtualLocation.subscribe(cb); 
+            return () => {
+                _window.removeEventListener('hashchange', hashChangeHandler);
+                return _locationUnsubscribe();
+            };
+        },
+
+        // set with location string
+        //
+        //      $location = '/foo/bar?baz=qux';
+        //
+        // note: must be absolute path
+
+        set(loc) {
+            if (!loc.startsWith('/')) return console.log(`can't set invalid location`);
+            _window.location.hash = '#' + loc;
+        }
+    }
+
+    export const is = (routeExpression, loc) =>
+        loc == null                           ? false
+      : routeExpression instanceof RegExp     ? routeExpression.test(loc.pathname)
+      : typeof routeExpression === 'function' ? routeExpression(location)
+      : typeof routeExpression === 'boolean'  ? routeExpression
+      :                                         routeExpression == loc.pathname;
+
+    export function match(loc, routes) {
+        let component;
+        for (const [ routeExpression, Component ] of routes) {
+                if (is(routeExpression, loc)) {
+                        component = Component;
+                        break;
+                }
+        }
+        return component;
+    }
+</script>
+
+<script>
+    export let routes = new Map(); // [ [ <routeExpression>, <Component> ] ]
+
+    $: pathname = $location?.pathname;
+    $: component = match($location, routes);
+</script>
+
+{#if component}
+    <svelte:component this={component} />
+{/if}
